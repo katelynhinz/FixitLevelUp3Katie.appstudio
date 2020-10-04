91e56de9d83927406949cf8f2cc5f024f2ab5a6b
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-04 16:23:36 -0500

    Update car.js

--- forms/car/car.js
+++ forms/car/car.js
@@ -3,13 +3,13 @@
   }
   
 
-  btnCalcCar.onclick=function carLoanpayment() {
+  btnCalcCar.onclick=function carLoanPayment() {
   let principleValue = inptPrinciple.value
   let monthlyInterest = inptMonthlyInterest.value
   let totalMonths = inptTotalMonths.value
   let carPayment =  carLoanPayment(Number(principleValue),Number(monthlyInterest),Number(totalMonths))
   lblResult.hidden = false
-  lblResult.textContent = ("A car loan for $ " + principleValue + " over " + totalMonths + " years at " + monthlyInterest +
+  lblResult.textContent = ("A car loan for $ " + principleValue + " over " + totalMonths + " months at " + monthlyInterest +
   " would have a monthly payment of " + carPayment)
 }
 
