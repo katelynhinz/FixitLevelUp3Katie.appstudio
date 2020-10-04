80db7c176c77d6e9c6ea3fe6905dafccbe5189af
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-04 16:23:40 -0500

    Update home.js

--- forms/home/home.js
+++ forms/home/home.js
@@ -1,24 +1,38 @@
-
-btn15.onclick=function(){
-  
-}
-
-btn30.onclick=function(){
-  
-}
-
-
-   function homeLoanPayment(p, r) {
-   return (p*(r*((1+r)**n))/(((1+r)**n)-1))
-  }
-  
-
-  btnCalcCar.onclick=function carLoanpayment() {
+   function homeLoanPayment15(p, r) {
+   return (p*(r*((1+r)**180))/(((1+r)**180)-1))
+ }
+    function homeLoanPayment30(p, r) {
+   return (p*(r*((1+r)**360))/(((1+r)**360)-1))
+ }
+ 
+ 
+ 
+ 
+btn15.onclick=function homeLoanPayment15() {
   let principleValue = inptPrinciple.value
   let monthlyInterest = inptMonthlyInterest.value
-  let totalMonths = inptTotalMonths.value
-  let carPayment =  carLoanPayment(Number(principleValue),Number(monthlyInterest),Number(totalMonths))
+  let homePayment =  homeLoanPayment15(Number(principleValue),Number(monthlyInterest))
   lblResult.hidden = false
-  lblResult.textContent = ("A car loan for $ " + principleValue + " over " + totalMonths + " years at " + monthlyInterest +
+  lblResult.textContent = ("A home loan for $ " + principleValue + " over " + totalMonths + " years at " + monthlyInterest +
   " would have a monthly payment of " + carPayment)
 }
+
+  
+
+
+
+
+
+btn30.onclick=function homeLoanPayment30(){
+  let principleValue = inptPrinciple.value
+  let monthlyInterest = inptMonthlyInterest.value
+  let homePayment =  homeLoanPayment30(Number(principleValue),Number(monthlyInterest))
+  lblResult.hidden = false
+  lblResult.textContent = ("A home loan for $ " + principleValue + " over " + totalMonths + " years at " + monthlyInterest +
+  " would have a monthly payment of " + carPayment)
+}
+
+
+
+
+ 
