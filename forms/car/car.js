6758f0f817cbbb68d4c0ba5f3f80c3e17fc88e3b
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-04 16:09:40 -0500

    Create car.js

---
+++ forms/car/car.js
@@ -0,0 +1,22 @@
+   function carLoanPayment(p, r, n) {
+   return (p*(r*((1+r)**n))/(((1+r)**n)-1))
+  }
+  
+
+  btnCalcCar.onclick=function carLoanpayment() {
+  let principleValue = inptPrinciple.value
+  let monthlyInterest = inptMonthlyInterest.value
+  let totalMonths = inptTotalMonths.value
+  let carPayment =  carLoanPayment(Number(principleValue),Number(monthlyInterest),Number(totalMonths))
+  lblResult.hidden = false
+  lblResult.textContent = ("A car loan for $ " + principleValue + " over " + totalMonths + " years at " + monthlyInterest +
+  " would have a monthly payment of " + carPayment)
+}
+
+
+
+/*
+     A home loan for $200,000 over 30 years at 5.7% interest would have a monthly payment of $1160.80.
+
+     A car loan for $30,000 over 60 months at 3.2% interest would have a monthly payment of $542.00
+*/
